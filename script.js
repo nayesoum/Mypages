03e2b581e2bf79dc28bfefba5e090d701829dec0
nayesoum <104022857+nayesoum@users.noreply.github.com>
2022-09-07 16:54:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let title = document.querySelector("title")
 title.innerHTML ="My pages"
 
-let title = document.querySelector("h1")
+let h1 = document.querySelector("h1")
 h1.innerHTML ="c'est ok pas de hello word"
